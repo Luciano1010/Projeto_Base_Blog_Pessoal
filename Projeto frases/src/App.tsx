e41a9374f07c014d8fc2d7d8bfc6d9301f0909d8
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2023-12-19 11:18:48 -0300

    parte visual da aplicação

--- Projeto frases/src/App.tsx
+++ Projeto frases/src/App.tsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
+import logoimg from "./assets/logo.png"
 
 function App() {
   
   return (
    
-     <div>
+     <div className="container">
+     <img 
+       src={logoimg}
+       alt="Logo frases" 
+       className="logo"/>
+    
+    <h2 className="title">Categorias</h2>
 
+    <section className="categoria-area">
+      <button className="categoria-button">Motivação</button>
+      <button className="categoria-button">Bem-Estar</button>
+    </section>
+
+    <button className="buttonfrase">Gerar Frase</button>
+    
+    <p className="textoFrase">Frases</p>
+    
     </div>
-   
+    
+  
 
   ) 
   
