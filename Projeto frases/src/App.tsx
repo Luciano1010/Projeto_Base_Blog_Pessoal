6165b9a54705c6ec44af0f653cd15b8de4ce938c
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2023-12-22 10:36:48 -0300

    Parte Interativa

--- Projeto frases/src/App.tsx
+++ Projeto frases/src/App.tsx
@@ -3,7 +3,38 @@ import "./App.css"
 import logoimg from "./assets/logo.png"
 
 function App() {
-  
+
+  const [textofrase, settextofrase] = useState("")
+  const[categoriaSelecionada, setCategoriaSelecionada]= useState(0)
+
+  const allFrases =[{
+    id:1,
+    nome:"Motivação",
+    frases:[
+      'Siga os bons',
+      'O bom senso é raro hoje',
+      'Nao deixe a sindrome do impostor te afetar'
+    ]
+  },{
+    id:2,
+    nome: "Bom dia",
+    frases:[
+      'acordar cedo é bom',
+      'acordar pra vida enquanto a tempo',
+      'muitos nao acordam pra vida'
+    ]
+  }
+  ]
+
+  function handleclickcategoria(index: number){
+    setCategoriaSelecionada(index);
+  }
+
+  function gerarfrase(){
+    let numeroAleatorio = Math.floor(Math.random() * allFrases[categoriaSelecionada].frases.length)
+      settextofrase(`"${allFrases[categoriaSelecionada].frases[numeroAleatorio]}"`)
+  }
+
   return (
    
      <div className="container">
@@ -15,13 +46,16 @@ function App() {
     <h2 className="title">Categorias</h2>
 
     <section className="categoria-area">
-      <button className="categoria-button">Motivação</button>
-      <button className="categoria-button">Bem-Estar</button>
+      {allFrases.map((item, index) =>(<button key={item.id}className="categoria-button" 
+      style={{borderWidth: item.nome === allFrases[categoriaSelecionada].nome ? 2 : 0, borderColor: "#1fa4db"}}
+      onClick={() => handleclickcategoria(index)}
+      >{item.nome}</button>))}
+      
     </section>
 
-    <button className="buttonfrase">Gerar Frase</button>
+    <button className="buttonfrase" onClick={gerarfrase}>Gerar Frase</button>
     
-    <p className="textoFrase">Frases</p>
+    {textofrase !== '' && <p className="textoFrase">{textofrase}</p>}
     
     </div>
     
